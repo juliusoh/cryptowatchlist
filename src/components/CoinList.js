6c530723540bb1aa0080e72d505f2ef5f3184596
julius oh <juliusoh@gmail.com>
2021-08-13 22:18:17 -0700

    api setup

--- src/components/CoinList.js
+++ src/components/CoinList.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import coinGecko from '../apis/coinGecko';
 
 const CoinList = () => {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await coinGecko.get("/coins/markets", {
+        params: {
+          vs_currency: "usd",
+          ids: "bitcoin, ethereum, cardano"
+        }
+      })
+
+      console.log(response.data)
+    }
+    fetchData();
+  }, [])
+
   return (
     <div>
-      
+
     </div>
   )
 }
