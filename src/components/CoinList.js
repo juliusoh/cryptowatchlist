2be328ef462b4812bbee73b7745af5944caa81a2
julius oh <juliusoh@gmail.com>
2021-08-13 22:27:32 -0700

    setup contextapi

--- src/components/CoinList.js
+++ src/components/CoinList.js
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import coinGecko from '../apis/coinGecko';
+import {WatchListContext} from '../context/WatchListContext';
 
 const CoinList = () => {
   const [coins, setCoins] = useState([]);
-
+  const {watchList} = useContext(WatchListContext)
+  console.log(watchList)
   useEffect(() => {
     const fetchData = async () => {
       const response = await coinGecko.get("/coins/markets", {
